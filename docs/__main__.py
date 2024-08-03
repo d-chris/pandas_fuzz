6757b36819da1c6bf2330dbb4aa6964282b52310
Christoph Dörrer <d-chris@web.de>
2024-08-03 09:14:35 +0200

    create documentation with warning json

--- docs/__main__.py
+++ docs/__main__.py
@@ -1,25 +1,91 @@
-import subprocess
+import json
+import warnings
 
 
-def main():
+class WarningMessageEncoder(json.JSONEncoder):
+    def default(self, obj):
+
+        if isinstance(obj, warnings.WarningMessage):
+            return {
+                k: v
+                for k, v in obj.__dict__.items()
+                if v is not None and not k.startswith("_")
+            }
+
+        if isinstance(obj, type):
+            return obj.__name__
+
+        if isinstance(obj, Exception):
+            return str(obj).strip().split("\n")
+
+        return super().default(obj)
+
+
+def main() -> int:
+    """
+    Create a static html documentation for the project in the 'public' directory.
+
+    Requires follwing PyPi packages:
+    - 'pdoc'
+    - 'pathlibutil'
+
+    Returns non-zero on failure.
+    """
+
     try:
-        subprocess.run(
-            [
-                "pdoc",
-                "-o",
-                "docs\\public",
-                "--no-show-source",
-                "-t",
-                "docs\\dark-mode",
+        from pathlibutil import Path
+        from pdoc import pdoc, render
+
+        with Path(__file__).parent as cwd:
+            print(f"\nrunning docs in {cwd=}...")
+
+            documentation = cwd / "public"
+
+            try:
+                print(f"cleaning output directory {documentation.size()}.")
+                documentation.delete(recursive=True, missing_ok=True)
+            except FileNotFoundError:
+                pass
+
+            documentation.mkdir(parents=True)
+
+            config = {
+                "template_directory": cwd / "dark-mode",
+                "show_source": False,
+                "search": False,
+            }
+
+            modules = [
                 "pandas_fuzz",
                 "pandas",
-            ],
-            encoding="utf-8",
-            check=True,
-        )
+                "rapidfuzz",
+            ]
+
+            with warnings.catch_warnings(record=True) as messages:
+                print(f"rendering {modules=}.")
+
+                render.configure(**config)
+                pdoc(*modules, output_directory=documentation)
+
+                log = documentation / "warnings.json"
+
+                with log.open("w", encoding="utf-8") as f:
+                    json.dump(
+                        messages,
+                        f,
+                        cls=WarningMessageEncoder,
+                    )
+
+                if messages:
+                    print(f"{log=} generated with {len(messages)} warnings.")
+    except ModuleNotFoundError as e:
+        print(f"Creation failed, due missing dependency!\n\tpip install {e.name}")
+        return 2
     except Exception as e:
-        print(str(e))
+        print(f"{documentation=} creation failed!\n\t{e}")
         return 1
+
+    print(f"{documentation=} generated successfully.\n")
 
     return 0
 
