e75fb55847fdc7249152449c0277a97cb095766f
Christoph Dörrer <d-chris@web.de>
2024-10-27 19:22:12 +0100

    created local pre-commit hooks
    - check docstrings for rapidfuzz funtions
    - rendering readme

---
+++ docs/__init__.py
@@ -0,0 +1,9 @@
+from .docstrings import main as check
+from .documentation import main as documentation
+from .readme import main as readme
+
+__all__ = [
+    "check",
+    "readme",
+    "documentation",
+]
--- pandas_fuzz/__init__.py
+++ pandas_fuzz/__init__.py
@@ -7,20 +7,24 @@ import rapidfuzz
 
 from .pdfuzz import FuzzDataFrameAccessor, FuzzSeriesAccessor
 
+__functions__ = [
+    func for func in rapidfuzz.fuzz.__all__ if callable(getattr(rapidfuzz.fuzz, func))
+]
+
 # Dynamically add methods to Accessors based on rapidfuzz_
-for method_name in rapidfuzz.fuzz.__all__:
+for method_name in __functions__:
     method = getattr(rapidfuzz.fuzz, method_name)
-    if callable(method):
-        setattr(
-            FuzzSeriesAccessor,
-            method_name,
-            FuzzSeriesAccessor._make_method(method),
-        )
-        setattr(
-            FuzzDataFrameAccessor,
-            method_name,
-            FuzzDataFrameAccessor._make_method(method),
-        )
+
+    setattr(
+        FuzzSeriesAccessor,
+        method_name,
+        FuzzSeriesAccessor._make_method(method),
+    )
+    setattr(
+        FuzzDataFrameAccessor,
+        method_name,
+        FuzzDataFrameAccessor._make_method(method),
+    )
 
 
 __all__ = [
